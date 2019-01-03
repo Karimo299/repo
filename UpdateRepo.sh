4dbac5aa46c6376aa6051b38e425a7cd136e0c18
Karimo299 <karimo299@Karims-MacBook-Pro.local>
2019-01-02 23:28:48 -0500

    Fixing script

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -10,7 +10,7 @@ do
   echo Filename: $file >> Packages
   echo Size: `wc -c < $file` >> Packages
   echo MD5sum: `MD5sum $file | cut -d '.' -f 1` >> Packages
-  echo \ >> Packages
+  echo>> Packages
   echo Finished `dpkg-deb -f $file Name` v`dpkg-deb -f $file Version`
 done
 echo -e "\033[32m"Finished Writing $num Packages "\033[0;39m"
