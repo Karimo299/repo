01180939b9da422ab0b845e007a8ff47fba51888
Karimo299 <karimo299@Karims-MacBook-Pro.local>
2019-01-02 23:16:16 -0500

    New Script for Packages.bz2

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+ #!/bin/bash
+rm -f ./Packages
+rm -f ./Packages.bz2
 
-rm Packages*
-./dpkg-scanpackages -m . /dev/null >Packages
+
+for file in ./deb/*
+do
+  num=$((num + 1))
+  dpkg-deb -f $file >> Packages
+  echo Filename: $file >> Packages
+  echo Size: `wc -c < $file` >> Packages
+  echo MD5sum: `MD5sum $file | cut -d '.' -f 1` >> Packages
+  echo \ >> Packages
+  echo Finished `dpkg-deb -f $file Name` v`dpkg-deb -f $file Version`
+done
+echo -e "\033[32m"Finished Writing $num Packages "\033[0;39m"
 bzip2 Packages
