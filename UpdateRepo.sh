a8179fdf7072431fdbf5962b12fbf1fec5027d52
Karimo299 <karimo299@Karims-MacBook-Pro.local>
2019-01-04 20:48:53 -0500

    Script Update

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,22 +1,30 @@
  #!/bin/bash
 rm -f ./Packages
 rm -f ./Packages.bz2
-# for file in ./deb/*
-# do
-#   num=$((num + 1))
-#   # dpkg-deb -f $file >> Packages
-#   echo dpkg-deb -f $file Name >>Packages
-  # echo dpkg-deb -f $file Package >>Packages
-#   echo dpkg-deb -f $file Version >>Packages
-#   echo dpkg-deb -f $file Architecture >>Packages
-#   echo Filename: $file >> Packages
-#   echo Size: `wc -c < $file` >> Packages
-#   echo MD5sum: `MD5sum $file | cut -d '.' -f 1` >> Packages
-#   echo>> Packages
-#   echo Finished `dpkg-deb -f $file Name` v`dpkg-deb -f $file Version`
-# echo `dpkg-deb -f $file Sileodepiction` >>Packages
-# done
-# echo -e "\033[32m"Finished Writing $num Packages "\033[0;39m"
+for file in ./*/*.deb
+do
+  num=$((num + 1))
+  echo Name: `dpkg-deb -f $file Name`>>Packages
+  echo Package: `dpkg-deb -f $file Package` >> Packages
+  echo Version: `dpkg-deb -f $file Version` >> Packages
+  echo Description: `dpkg-deb -f $file Description` >> Packages
+  echo Architecture: `dpkg-deb -f $file Architecture` >> Packages
+  echo Author: `dpkg-deb -f $file Author` >> Packages
+  echo Maintainer: `dpkg-deb -f $file Maintainer` >> Packages
+  echo Installed-Size: `dpkg-deb -f $file Installed-Size` >> Packages
+  echo Depends: `dpkg-deb -f $file Depends` >> Packages
+  echo Filename: $file >> Packages
+  echo Size: `wc -c < $file` >> Packages
+  echo MD5sum: `MD5sum $file | cut -d '.' -f 1` >> Packages
+  echo SHA1: `sha1sum $file | cut -d '.' -f 1` >> Packages
+  echo SHA256: `shasum -a 256 $file | cut -d '.' -f 1` >> Packages
+  echo Section: `dpkg-deb -f $file Section` >>Packages
+  echo Description: `dpkg-deb -f $file Description` >>Packages
+  echo Depiction: https://karimo299.github.io/repo/depictions/?p=`dpkg-deb -f $file package` >>Packages
+  echo SileoDepiction: https://karimo299.github.io/repo/sileo/`dpkg-deb -f $file package` >>Packages
+  echo>> Packages
+  echo Finished `dpkg-deb -f $file Name` v`dpkg-deb -f $file Version`
+done
 
-dpkg-scanpackages -m ./deb > Packages
+echo -e "\033[32m"Finished Writing $num Packages "\033[0;39m"
 bzip2 Packages
