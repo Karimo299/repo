8b763709f1f71bb03cfb1e7ef161895f667e21b9
Karimo299 <karimo299@Karims-MacBook-Pro.local>
2019-01-08 22:16:29 -0500

    CCRinger Update 1.0.3

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,31 +1,33 @@
  #!/bin/bash
-rm -f ./Packages
-rm -f ./Packages.bz2
-for file in ./*/*.deb
+repo="$( cd "$(dirname "$0")" ; pwd -P )"
+rm -f $repo/Packages
+rm -f $repo/Packages.bz2
+for file in $repo/*/*.deb
 do
   num=$((num + 1))
-  echo Name: `dpkg-deb -f $file Name`>>Packages
-  echo Package: `dpkg-deb -f $file Package` >> Packages
-  echo Version: `dpkg-deb -f $file Version` >> Packages
-  echo Description: `dpkg-deb -f $file Description` >> Packages
-  echo Architecture: `dpkg-deb -f $file Architecture` >> Packages
-  echo Author: `dpkg-deb -f $file Author` >> Packages
-  echo Maintainer: `dpkg-deb -f $file Maintainer` >> Packages
-  echo Installed-Size: `dpkg-deb -f $file Installed-Size` >> Packages
-  echo Depends: `dpkg-deb -f $file Depends` >> Packages
-  echo Filename: $file >> Packages
-  echo Size: `wc -c < $file` >> Packages
-  echo MD5sum: `MD5sum $file | cut -d '.' -f 1` >> Packages
-  echo SHA1: `sha1sum $file | cut -d '.' -f 1` >> Packages
-  echo SHA256: `shasum -a 256 $file | cut -d '.' -f 1` >> Packages
-  echo Section: `dpkg-deb -f $file Section` >>Packages
-  echo Description: `dpkg-deb -f $file Description` >>Packages
-  echo Depiction: https://karimo299.github.io/repo/depictions/?p=`dpkg-deb -f $file package` >>Packages
-  echo SileoDepiction: https://karimo299.github.io/repo/sileo/`dpkg-deb -f $file package` >>Packages
-  echo Icon: https://karimo299.github.io/repo/icons/`dpkg-deb -f $file Name`.png >>Packages
-  echo>> Packages
+  echo Name: `dpkg-deb -f $file Name`>>$repo/Packages
+  echo Package: `dpkg-deb -f $file Package` >> $repo/Packages
+  echo Version: `dpkg-deb -f $file Version` >> $repo/Packages
+  echo Description: `dpkg-deb -f $file Description` >> $repo/Packages
+  echo Architecture: `dpkg-deb -f $file Architecture` >> $repo/Packages
+  echo Author: `dpkg-deb -f $file Author` >> $repo/Packages
+  echo Author: `dpkg-deb -f $file Author` >> $repo/Packages
+  echo Maintainer: `dpkg-deb -f $file Maintainer` >> $repo/Packages
+  echo Installed-Size: `dpkg-deb -f $file Installed-Size` >> $repo/Packages
+  echo Depends: `dpkg-deb -f $file Depends` >> $repo/Packages
+  echo Filename: $file >> $repo/Packages
+  echo Size: `wc -c < $file` >> $repo/Packages
+  echo MD5sum: `MD5sum $file | cut -d '.' -f 1` >> $repo/Packages
+  echo SHA1: `sha1sum $file | cut -d '.' -f 1` >> $repo/Packages
+  echo SHA256: `shasum -a 256 $file | cut -d '.' -f 1` >> $repo/Packages
+  echo Section: `dpkg-deb -f $file Section` >>$repo/Packages
+  echo Description: `dpkg-deb -f $file Description` >>$repo/Packages
+  echo Depiction: https://karimo299.github.io/repo/depictions/?p=`dpkg-deb -f $file package` >>$repo/Packages
+  echo SileoDepiction: https://karimo299.github.io/repo/sileo/`dpkg-deb -f $file package` >>$repo/Packages
+  echo Icon: https://karimo299.github.io/repo/icons/`dpkg-deb -f $file Name`.png >>$repo/Packages
+  echo>> $repo/Packages
   echo Finished `dpkg-deb -f $file Name` v`dpkg-deb -f $file Version`
 done
 
 echo -e "\033[32m"Finished Writing $num Packages "\033[0;39m"
-bzip2 Packages
+bzip2 $repo/Packages
